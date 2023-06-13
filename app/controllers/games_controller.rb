44ffde84565088b9e3e04175c1c4aea340be328a
Jun Ukemori <jukemori@gmail.com>
2023-06-13 20:08:43 +0900

    style added

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,27 +2,31 @@ require "json"
 require "open-uri"
 
 class GamesController < ApplicationController
-  def new
-    @letters = ('A'..'Z').to_a.sample(10)
-  end
-  def score
-    p word = params[:word]
-    url = "https://wagon-dictionary.herokuapp.com/#{word.downcase}"
-    json = URI.open(url).read
-    dictionary = JSON.parse(json)
 
-  if included?(word, params[:letters]) == false
-    @response = "Sorry but test can't be built out of #{params[:letters]}"
-  elsif dictionary['found'] == false
-    @response = "Sorry but #{word.upcase} does not seem to be a valid English word..."
-  elsif dictionary['found'] == true
-    @response = "Congraturations! #{word.upcase} is a valid English word!"
+  VOWELS = %w(A E I O U Y)
+
+  def new
+    @letters = Array.new(5) { VOWELS.sample }
+    @letters += Array.new(5) { (('A'..'Z').to_a - VOWELS).sample }
+    @letters.shuffle!
   end
+
+  def score
+    @letters = params[:letters].split
+    @word = (params[:word] || "").upcase
+    @included = included?(@word, @letters)
+    @english_word = english_word?(@word)
   end
 
   private
 
-  def included?(guess, grid)
-    guess.chars.all? { |letter| guess.count(letter) <= grid.count(letter) }
+  def included?(word, letters)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
+  end
+
+  def english_word?(word)
+    response = URI.open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
   end
 end
